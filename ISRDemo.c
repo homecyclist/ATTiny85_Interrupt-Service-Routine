5c5a6fc629e1fc85cdab2766a454ec47c878c719
homecyclist <49373727+homecyclist@users.noreply.github.com>
2021-03-06 19:28:34 -0500

    Clean up comments and rename main.c to ISRDemo.c

--- ISRDemo.c
+++ ISRDemo.c
@@ -15,10 +15,10 @@ void timer0_init(void)
 	/*
 	This timer will fire ISR(TIMER0_COMPA_vect) on match.
 	*/
-	TCCR0A = (1<<WGM01);     //CTC mode. Count until OCR0A, then reset.
-	TCCR0B = (3<<CS00);      //F_CPU / 64
-	OCR0A  = 249;			 //1 ms
-	TIMSK |= (1<<OCIE0A);    //Call ISR(TIMER0_COMPA_vect) on match
+	TCCR0A = (1<<WGM01);	//CTC mode. Count until OCR0A, then reset.
+	TCCR0B = (3<<CS00);	//F_CPU / 64
+	OCR0A  = 249;		//1 ms
+	TIMSK |= (1<<OCIE0A);	//Call ISR(TIMER0_COMPA_vect) on match
 }
 
 void timer1_init(void)
@@ -26,9 +26,9 @@ void timer1_init(void)
 	/*
 	This timer will fire ISR(TIMER1_COMPA_vect) on match.
 	*/
-	TCCR1 |= (1<<CTC1);		//CTC mode. Count until OCR1C, then reset
-	TCCR1 |= (8<<CS10);		//F_CPU / 64
-	OCR1C = 124;			//1 ms
+	TCCR1 |= (1<<CTC1);	//CTC mode. Count until OCR1C, then reset
+	TCCR1 |= (8<<CS10);	//F_CPU / 64
+	OCR1C = 124;		//1 ms
 	TIMSK |= (1<<OCIE1A);	//Call ISR(TIMER1_COMPA_vect) on match
 }
 
@@ -37,8 +37,8 @@ int main(void)
 	DDRB = (1<<DDB4) | (1<<DDB3);
 	timer0_init();
 	timer1_init();
-    sei ();        // allow interrupts
-    while(1) 
+	sei ();			// allow interrupts
+	while(1) 
 	{
 	// Nothing is needed here!
 	}  
@@ -47,12 +47,10 @@ int main(void)
 
 ISR(TIMER0_COMPA_vect)
 {
-	// toggle pin 3
-	PORTB ^= (1<<PB4);   
+	PORTB ^= (1<<PB4);	// toggle pin 3  
 }
 
 ISR(TIMER1_COMPA_vect)
 {
-	// toggle pin 4
-	PORTB ^= (1<<PB3); 
+	PORTB ^= (1<<PB3);	// toggle pin 4 
 }
